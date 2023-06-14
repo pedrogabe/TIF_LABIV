a87b7097bf02b17d016bc0aff4b7ff30270e93d1
Pedro García Beguiristain <pedro.garcia@sarsy-srl.com>
2023-06-14 15:52:49 -0300

    Se renombra servlet ABMPacientes

--- src/main/java/servlets/ABMPacientes.java
+++ src/main/java/servlets/ABMPacientes.java
@@ -14,13 +14,13 @@ import entidad.Paciente;
  * Servlet implementation class Pacientes
  */
 @WebServlet("/Pacientes")
-public class Pacientes extends HttpServlet {
+public class ABMPacientes extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Pacientes() {
+    public ABMPacientes() {
         super();
         // TODO Auto-generated constructor stub
     }
