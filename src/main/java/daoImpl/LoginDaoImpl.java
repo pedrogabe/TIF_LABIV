11b5c171cb9e69f4fde255222b72a661de0bb60f
Gabriel Vargas <94945902+GabrielV6@users.noreply.github.com>
2023-06-15 11:40:28 -0300

    LoginDaoImpl
    Logica para recuperar usuario de la Base y posterior validacion en Login.

---
+++ src/main/java/daoImpl/LoginDaoImpl.java
@@ -0,0 +1,49 @@
+package daoImpl;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+import dao.LoginDao;
+import entidad.Usuario;
+
+public class LoginDaoImpl implements LoginDao {
+	
+	private static final String SELECT_USER = "select * from clinica_medica.usuarios where UserLogin=? and Password=?";
+	
+	@Override
+	public Usuario readUser(String usuario, String password) {
+		PreparedStatement statement;
+		ResultSet resultSet;
+		Usuario usuar = null;
+		Conexion conexion = Conexion.getConexion();
+		
+		try {
+			statement = conexion.getSQLConexion().prepareStatement(SELECT_USER);
+			statement.setString(1, usuario.trim());
+			statement.setString(2, password.trim());
+			
+			resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				usuar = getUsuario(resultSet);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return usuar;
+	}
+	
+	private Usuario getUsuario(ResultSet resultSet) throws SQLException {
+		int IdUsuario = resultSet.getInt("IdUsuario");
+		String UserLogin = resultSet.getString("UserLogin");
+		String Password = resultSet.getString("Password");
+		int IdPerfil = resultSet.getInt("IdPerfil");
+		
+		return new Usuario(IdUsuario,UserLogin,Password,IdPerfil);
+	}
+	
+	
+
+}
