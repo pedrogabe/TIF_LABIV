f350ccb1cdb69afc6b19da40588eb32025e3f31d
Roberto Rangogni <rob_rang@hotmail.com>
2023-06-16 22:03:56 -0300

    Update Script.sql

--- src/main/ScripDB/Script.sql
+++ src/main/ScripDB/Script.sql
@@ -58,6 +58,7 @@ CREATE TABLE Pacientes (
     IdNacionalidad INT NOT NULL,
     FechaNacimiento DATETIME NOT NULL,
     Direccion VARCHAR(50) NOT NULL,
+    IdProvincia INT NOT NULL,
     IdLocalidad INT NOT NULL,
     CorreoElectronico VARCHAR(50) NOT NULL,
     Telefono VARCHAR(50) NOT NULL,
@@ -65,6 +66,7 @@ CREATE TABLE Pacientes (
     Estado BIT NOT NULL,
     CONSTRAINT FK_PacientePerfil FOREIGN KEY (IdPerfil) REFERENCES Perfiles(IdPerfil),
     CONSTRAINT FK_PacienteNacionalidad FOREIGN KEY (IdNacionalidad) REFERENCES Nacionalidades(IdNacionalidad),
+    CONSTRAINT FK_PacienteProvincia FOREIGN KEY (IdProvincia) REFERENCES Provincia(IdProvincia),
     CONSTRAINT FK_PacienteLocalidad FOREIGN KEY (IdLocalidad) REFERENCES Localidades(IdLocalidad)
 );
 
