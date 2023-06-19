f9ba8e7723f8dc7c67a2e0fd6daed512fac8b8fd
Roberto Rangogni <rob_rang@hotmail.com>
2023-06-19 11:57:54 -0300

    Add files via upload

---
+++ src/main/java/dao/MedicoDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.ArrayList;
+
+import entidad.Medico;
+
+public interface MedicoDao {
+	public boolean insert(Medico medico);
+	public boolean update(Medico medico, boolean esBaja);
+	public ArrayList<Medico> readAll(int estado);
+	public Medico searchMedico(int dni);
+	public int selectCount(Medico medico);
+}
