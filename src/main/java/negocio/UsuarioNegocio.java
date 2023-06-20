fa01b873ea72b56141645ddc7c044cf94d8ebd44
Roberto Rangogni <rob_rang@hotmail.com>
2023-06-20 12:36:48 -0300

    Add files via upload

---
+++ src/main/java/negocio/UsuarioNegocio.java
@@ -0,0 +1,13 @@
+package negocio;
+
+import java.util.ArrayList;
+
+import entidad.Usuario;
+
+public interface UsuarioNegocio {
+	public boolean insert(Usuario usuario);
+	public boolean delete(Usuario usuario);
+	public boolean update(Usuario usuario);
+	public ArrayList<Usuario> readAll(int estado);
+	public boolean exists(Usuario usuario);
+}
