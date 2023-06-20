918f2722dfe82917a677f4fe243c59720f593803
Roberto Rangogni <rob_rang@hotmail.com>
2023-06-20 12:37:18 -0300

    Add files via upload

---
+++ src/main/java/negocioImpl/UsuarioNegocioImpl.java
@@ -0,0 +1,49 @@
+package negocioImpl;
+
+import java.util.ArrayList;
+
+import dao.UsuarioDao;
+import daoImpl.UsuarioDaoImpl;
+import entidad.Usuario;
+import negocio.UsuarioNegocio;
+
+public class UsuarioNegocioImpl implements UsuarioNegocio{
+
+	UsuarioDao usuarioDaoImp = null;
+	
+	@Override
+	public boolean insert(Usuario usuario) {
+		usuarioDaoImp = new UsuarioDaoImpl();
+		boolean insertado = false;
+		if (usuario.getUserLogin().trim() != "" && usuario.getPassword().trim() != ""
+				&& usuario.getIdPerfil() > 0) {
+			insertado = usuarioDaoImp.insert(usuario);
+		}
+		return insertado;
+	}
+
+	@Override
+	public boolean delete(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<Usuario> readAll(int estado) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean exists(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
