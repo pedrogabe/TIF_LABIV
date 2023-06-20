cc1d7d5906d9c557991ef01aa0299d84032037d0
Roberto Rangogni <rob_rang@hotmail.com>
2023-06-20 14:24:37 -0300

    Add files via upload

--- src/main/java/dao/UsuarioDao.java
+++ src/main/java/dao/UsuarioDao.java
@@ -8,7 +8,8 @@ public interface UsuarioDao {
 	public boolean insert(Usuario usuario);
 	public boolean update(Usuario usuario, boolean esBaja);
 	public ArrayList<Usuario> readAll(int usuario);
-	public Usuario searchUsuario(String userLogin);
+	public Usuario searchUsuario(int idUsuario);
 	public int selectCount(Usuario usuario);
+	public int selectMaxId();
 
 }
