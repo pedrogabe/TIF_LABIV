4d27a5e8c5eb57bdc07927a313038a366571bd7d
Jose <84431245+jocmamorales@users.noreply.github.com>
2023-06-21 23:30:36 -0300

    cambios
    Subo TurnoDao Completo ok,

---
+++ src/main/java/dao/TurnoDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.ArrayList;
+
+import entidad.Turno;
+
+public interface TurnoDao {
+	public boolean insert(Turno turno);
+	public boolean update(Turno turno, boolean esBaja);
+	public ArrayList<Turno> readAll(int estado);
+	public Turno searchTurno(int idTurno);
+	public int selectCount(Turno turno);
+}
