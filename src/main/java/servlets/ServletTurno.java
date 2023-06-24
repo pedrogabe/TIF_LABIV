cc98c4f593901b894bfc43d97f66fe723e73b0bd
tfdace <58187350+tfdace@users.noreply.github.com>
2023-06-24 21:30:10 +0200

    se modifica servlet para cargar turno seleccionado

--- src/main/java/servlets/ServletTurno.java
+++ src/main/java/servlets/ServletTurno.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import entidad.Especialidad;
+import entidad.Paciente;
+import entidad.Turno;
 import negocio.*;
 import negocioImpl.*;
 
@@ -35,13 +38,53 @@ public class ServletTurno extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		if (request.getSession().getAttribute("Usuario") != null) {
+			String op = request.getParameter("op");
+			if (op == null && request.getAttribute("op") != null)
+				op = request.getAttribute("op").toString();
+			if (op == null || !(op.equals("add") || op.equals("edit") || op.equals("delete"))) {
+				response.sendError(400);
+				System.out.println("op catch");
+				return;
+			}
+		
 		EspecialidadNegocio espNegImp = new EspecialidadNegocioImpl();
 		ArrayList<Especialidad> especialidades = espNegImp.readAll();
 		
 		request.setAttribute("especialidades", especialidades);
 		
+		
+		if (op.equals("add")) {
+
+		} else {
+			int id;
+			try {
+				if (request.getParameter("id") != null)
+					id = Integer.parseInt(request.getParameter("id"));
+				else
+					id = Integer.parseInt(request.getAttribute("id").toString());
+			} catch (Exception e) {
+				response.sendError(400);
+				System.out.println("id catch");
+				return;
+			}
+
+			try {
+				Turno turno = getTurno(request, id);
+				request.setAttribute("turno", turno);
+			} catch (Exception e) {
+				response.sendError(500);
+				return;
+			}
+		}
+
 		RequestDispatcher rd = request.getRequestDispatcher("ABMLTurno.jsp");
+		request.setAttribute("op", op);
 		rd.forward(request, response);
+	} else {
+	    response.sendRedirect(request.getContextPath() + "/Error.jsp");
+	}
+
 	}
 
 	/**
@@ -50,6 +93,22 @@ public class ServletTurno extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	protected Turno getTurno(HttpServletRequest request, int id) {
+		Object ps = request.getSession().getAttribute("turnos");
+		
+		if (ps != null) {
+			@SuppressWarnings("unchecked")
+			List<Turno> turnos = (List<Turno>) request.getSession().getAttribute("turnos");
+			for (Turno turno : turnos) {
+				if (turno.getIdTurno() == id)
+					return turno;
+			}
+			request.setAttribute("error", "No se encontró el turno solicitado en la base de datos.");		
+		}
+		
+		return null;
 	}
 
 }
