dce7790ab9b9ce40f53f8bae6c373cb658191344
tfdace <58187350+tfdace@users.noreply.github.com>
2023-06-24 05:17:49 +0200

    Add files via upload

---
+++ src/main/java/servlets/ServletListarTurnos.java
@@ -0,0 +1,61 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import negocio.TurnosNegocio;
+import negocioImpl.TurnosNegocioImpl;
+
+/**
+ * Servlet implementation class ServletListarTurnos
+ */
+@WebServlet("/ServletListarTurnos")
+public class ServletListarTurnos extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static TurnosNegocio turnoNeg = null;
+  
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletListarTurnos() {
+    	turnoNeg = new TurnosNegocioImpl();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getSession().getAttribute("Usuario") != null) {
+			response.getWriter().append("Served at: ").append(request.getContextPath());
+			try {
+				request.getSession().setAttribute("turnos", turnoNeg.readAll(1)); 
+			} catch (Exception e) {
+				e.printStackTrace();
+				request.setAttribute("error", "Hubo un problema al intentar la lista de turnos");
+			}
+			RequestDispatcher rd = request.getRequestDispatcher("ListarTurnos.jsp");
+			rd.forward(request, response);
+		} else {
+			PrintWriter out = response.getWriter();
+			response.setContentType("text/html");
+			out.println("<font color=red size18>No tiene autorizacion, debe ingresar con usuario!<br>");
+			out.println("<a href=Login.jsp>Ir al Login!</a>");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
