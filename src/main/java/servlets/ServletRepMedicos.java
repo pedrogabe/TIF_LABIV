061519bfca92e62c37dccc9af6d30dfd5f8b7611
tfdace <58187350+tfdace@users.noreply.github.com>
2023-06-29 06:20:54 +0200

    se agrega reporte de turnos por medicos

--- src/main/java/servlets/ServletRepMedicos.java
+++ src/main/java/servlets/ServletRepMedicos.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import entidad.ReporteMedico;
+import entidad.Usuario;
+import negocio.RepMedicoNegocio;
+import negocioImpl.RepMedicoNegocioImpl;
 
 /**
  * Servlet implementation class ServletRepMedicos
@@ -28,16 +34,51 @@ public class ServletRepMedicos extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("ReporteMedicos.jsp");
-		rd.forward(request, response);
+		
+		Usuario usuario = (Usuario) request.getSession().getAttribute("Usuario");
+
+		if (request.getSession().getAttribute("Usuario") != null && usuario.getIdPerfil() != Usuario.ROL_MEDICO) {
+			response.getWriter().append("Served at: ").append(request.getContextPath());
+			try {
+				// Cargar controles de la pagina
+			} catch (Exception e) {
+				e.printStackTrace();
+				request.setAttribute("error", "Hubo un problema al cargar la pantalla del reporte");
+			}
+			RequestDispatcher rd = request.getRequestDispatcher("ReporteMedicos.jsp");
+			rd.forward(request, response);
+		} else {
+			response.sendRedirect(request.getContextPath() + "/Error.jsp");
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		if (request.getParameter("mesMedico") != null) {
+
+			String anioMes = (request.getParameter("mesMedico").toString());
+			RepMedicoNegocio repMed = new RepMedicoNegocioImpl();
+
+			try {
+
+				ArrayList<ReporteMedico> report = repMed.searchReport(anioMes);
+				
+				if (report != null ) {
+					request.setAttribute("reporte", report);
+					request.setAttribute("anioMes", anioMes);				
+				}
+				else {
+					request.setAttribute("error", "No se encontraron datos para el reporte");
+				}
+				RequestDispatcher rd = request.getRequestDispatcher("ReporteMedicos.jsp");
+				rd.forward(request, response);
+
+			} catch (Exception e) {
+				response.sendRedirect(request.getContextPath() + "/Error.jsp");
+			}
+		}
 	}
 
 }
